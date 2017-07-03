ebc16d275770da033e99e48efced04106233f103
wangtao <taowang9@creditharmony.cn>
2017-07-03 14:38:17 +0800

    1

---
+++ src/main/java/orgs/cm/pjOpStAuth/App.java
@@ -0,0 +1,13 @@
+package orgs.cm.pjOpStAuth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
