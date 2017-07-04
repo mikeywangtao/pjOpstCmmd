1bb616057621bf70135ebee257b48b1909f2e88b
wangtao <taowang9@creditharmony.cn>
2017-07-04 10:27:13 +0800

    1

--- src/main/java/orgs/cm/tst/dao/TstDao.java
+++ src/main/java/orgs/cm/tst/dao/TstDao.java
@@ -1,7 +1,11 @@
 package orgs.cm.tst.dao;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("tstDao")
@@ -9,11 +13,20 @@ public class TstDao {
 
 	private String strCname = TstDao.class.getName();
 	
+	@Autowired
+	private SqlSessionFactory  sqlSessionFactory ;
+	
 	public String disTstDao_Pro(LinkedHashMap<String, Object> lhpParp){
 		String strFname = " disTstDao_Pro : ";
 		String strRe = "";
-		
-		System.out.println(strCname + strFname);
+		try {
+			SqlSession session = sqlSessionFactory.openSession();
+			List lstRe = session.selectList("orgs.cm.tst.dao.TstDao.getTst");
+			System.out.println(strCname + strFname);
+		} catch(Exception ex) {
+			System.out.println(strCname + strFname + ex);
+		}
+
 		
 		return strRe;
 	}
